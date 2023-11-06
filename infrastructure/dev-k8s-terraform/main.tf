a7217ebef2f493a3006c9036cabbacc9172ba071
rsametk <rsmtkcky@gmail.com>
2023-11-06 17:08:47 +0000

    keypair

--- infrastructure/dev-k8s-terraform/main.tf
+++ infrastructure/dev-k8s-terraform/main.tf
@@ -157,7 +157,7 @@ resource "aws_instance" "kube-master" {
     instance_type = "t3a.medium"
     iam_instance_profile = aws_iam_instance_profile.petclinic-master-server-profile.name
     vpc_security_group_ids = [aws_security_group.petclinic-kube-master-sg.id, aws_security_group.petclinic-mutual-sg.id]
-    key_name = "my_keyname"
+    key_name = "clarus"
     subnet_id = "subnet-030f2c2c321e6fdf7"  # select own subnet_id of us-east-1a
     availability_zone = "us-east-1a"
     tags = {
